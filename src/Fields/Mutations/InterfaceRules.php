f102a97b7069bf14cb738a1802d4e443d0eff88d
Marek Gogol <9mike66@gmail.com>
2017-06-21 18:15:17 +0200

    - pridana featura obmedzovania atributov pre frontend, backend a konzolu.

---
+++ src/Fields/Mutations/InterfaceRules.php
@@ -0,0 +1,56 @@
+<?php
+namespace Gogol\Admin\Fields\Mutations;
+
+use Fields;
+use Admin;
+
+class InterfaceRules
+{
+    public $attributes = ['in_frontend', 'in_backend', 'in_admin', 'in_console'];
+
+    public function update( $field )
+    {
+        foreach ($this->attributes as $attribute)
+        {
+            if ( array_key_exists($attribute, $field) )
+            {
+                if ( $this->canRegisterRules($attribute) )
+                    $field = $this->registerAttributes($field[$attribute], $field);
+
+                unset($field[$attribute]);
+            }
+
+        }
+
+
+        return $field;
+    }
+
+    /*
+     * Check if fields can be registrated in actual interface
+     */
+    private function canRegisterRules($type)
+    {
+        if ( $type == 'in_frontend' && Admin::isFrontend() )
+            return true;
+
+        if ( in_array($type, ['in_backend', 'in_admin']) && Admin::isAdmin() )
+            return true;
+
+        if ( $type == 'in_console' && app()->runningInConsole() )
+            return true;
+
+        return false;
+    }
+
+    /*
+     * Add rules info fields
+     */
+    private function registerAttributes($rules, $field)
+    {
+        $rules = is_array($rules) ? $rules : [$rules];
+
+        return $field + Fields::mutate(FieldToArray::class, $rules);
+    }
+}
+?>
