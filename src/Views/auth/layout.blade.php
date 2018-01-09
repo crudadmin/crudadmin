9067cb9d9cef931d6369df0d726a0cbc8a01dc90
Marek Gogol <9mike66@gmail.com>
2018-01-09 12:05:27 +0100

    - added multiple admin models paths support with multiple namespaces

--- src/Views/auth/layout.blade.php
+++ src/Views/auth/layout.blade.php
@@ -7,17 +7,17 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="{{ admin_asset('admin/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ admin_asset('bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ admin_asset('admin/dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ admin_asset('dist/css/AdminLTE.min.css') }}">
   <!-- Admin css -->
-  <link rel="stylesheet" href="{{ admin_asset('admin/css/style.css') }}">
+  <link rel="stylesheet" href="{{ admin_asset('css/style.css') }}">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{ admin_asset('admin/plugins/iCheck/square/blue.css') }}">
+  <link rel="stylesheet" href="{{ admin_asset('plugins/iCheck/square/blue.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,10 +38,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.1.4 -->
-<script src="{{ admin_asset('admin/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-<script src="{{ admin_asset('admin/plugins/iCheck/icheck.min.js') }}"></script>
+<script src="{{ admin_asset('plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+<script src="{{ admin_asset('plugins/iCheck/icheck.min.js') }}"></script>
 <!-- Bootstrap 3.3.5 -->
-<script src="{{ admin_asset('admin/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ admin_asset('bootstrap/js/bootstrap.min.js') }}"></script>
 <script>
   $(function () {
     $('input').iCheck({
