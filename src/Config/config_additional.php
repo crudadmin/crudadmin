d7c94dbc45f189a6170183496e2bca0c2e5725b9
Marek Gogol <9mike66@gmail.com>
2019-07-08 13:12:00 +0200

    fixed models paths

--- src/Config/config_additional.php
+++ src/Config/config_additional.php
@@ -9,9 +9,9 @@ return [
      * From which directories CrudAdmin should load Admin Modules
      */
     'models' => [
-        app_path() => 'App',
-        app_path('Model') => 'App/Model',
-        app_path('Eloquent') => 'App/Eloquent',
+        'App' => app_path(),
+        'App/Model' => app_path('Model'),
+        'App/Eloquent' => app_path('Eloquent'),
     ],
 
     /*
