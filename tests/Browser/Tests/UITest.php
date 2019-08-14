30892079abdebb718a231435e2a2b918ad89bc89
Marek Gogol <9mike66@gmail.com>
2019-08-14 14:21:13 +0200

    added test for public assets version difference error

---
+++ tests/Browser/Tests/UITest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Admin\Tests\Browser\Tests;
+
+use Admin;
+use Admin\Tests\App\User;
+use Admin\Tests\Browser\DuskBrowser;
+use Admin\Tests\Browser\BrowserTestCase;
+
+class UITest extends BrowserTestCase
+{
+    /** @test */
+    public function test_assets_version_check_alert()
+    {
+        $this->browse(function (DuskBrowser $browser) {
+            file_put_contents(Admin::getAssetsVersionPath('version.txt'), 'wrong-version');
+
+            $browser->loginAs(User::first())
+                    ->visit(admin_action('DashboardController@index'))
+                    ->assertSee('php artisan admin:update')
+                    ->assertSee('wrong-version');
+        });
+    }
+}
