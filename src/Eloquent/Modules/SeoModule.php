21747ffdc2f2160e99c37631a42ff37c741f5cb8
Marek Gogol <9mike66@gmail.com>
2020-01-28 18:29:11 +0100

    added custom slugs support into seo module

--- src/Eloquent/Modules/SeoModule.php
+++ src/Eloquent/Modules/SeoModule.php
@@ -2,23 +2,36 @@
 
 namespace Admin\Eloquent\Modules;
 
-use Admin\Core\Eloquent\AdminModelModule;
+use Admin\Core\Eloquent\Concerns\AdminModelModule;
+use Admin\Core\Eloquent\Concerns\AdminModelModuleSupport;
 use Admin\Fields\Group;
 
-class SeoModule implements AdminModelModule
+class SeoModule extends AdminModelModule implements AdminModelModuleSupport
 {
     public function isActive($model)
     {
         return $model->getProperty('seo') === true;
     }
 
-    public function mutateFields($fields)
+    public function mutateFields($fields, $row)
     {
-        $fields->push(Group::tab([
+        $model = $this->getModel();
+
+        $items = [];
+
+        //Add sluggable column settings
+        if ( $model->hasSluggable() ) {
+            $items = array_merge($items, [Group::fields([
+                'slug' => 'name:Url adresa|required|readonlyIf:slug_dynamic,1|unique:'.$model->getTable().',slug,'.(isset($row) ? $row->getKey() : 'NULL').',id,deleted_at,NULL',
+                'slug_dynamic' => 'name:Automatická url adresa|type:checkbox|default:1',
+            ])->inline()->add('hidden')]);
+        }
+
+        $fields->push(Group::tab(array_merge($items, [
             'meta_title' => 'name:Titulok stránky',
             'meta_keywords' => 'name:Kľúčové slova',
             'meta_description' => 'name:Popis stránky|type:text|max:400',
             'meta_image' => 'name:Obrázky stránky|image|multiple',
-        ])->add('hidden')->name('Meta tagy')->icon('fa-info'));
+        ]))->add('hidden')->name('Meta tagy')->icon('fa-info'));
     }
 }
