3b956685ce042b54c7d555400c8509f66e2e29e7
Marek Gogoľ <info@marekgogol.sk>
2020-12-01 14:22:19 +0100

    updated excel download

--- src/Helpers/SheetDownloader.php
+++ src/Helpers/SheetDownloader.php
@@ -42,6 +42,57 @@ class SheetDownloader
         return $directory;
     }
 
+    private function getBelongsToValue($column, $value)
+    {
+        $field = $this->model->getField($column);
+        $options = @$field['options'];
+
+        $belongsTo = @$field['belongsTo']?:$field['belongsToMany'];
+        $belongsTo = str_replace(',', '.', $belongsTo);
+        $belongsTo = explode('.', $belongsTo);
+        $belongsTo = array_slice($belongsTo, 1);
+        $belongsTo = implode('.', $belongsTo);
+
+        $ids = array_wrap($value);
+        $array = [];
+
+        foreach ($ids as $id) {
+            $option = @$options[$id];
+
+            //If select option does not exists
+            if ( !$option ){
+                continue;
+            }
+
+            if ( array_key_exists($belongsTo, $option) ){
+                $array[] = $option[$belongsTo];
+            } else {
+                $string = $belongsTo;
+
+                foreach ($option as $key => $v) {
+                    $string = str_replace(':'.$key, $v, $string);
+                }
+
+                if ( $string !== $belongsTo ){
+                    $array[] = $string;
+                }
+            }
+        }
+
+        return implode(', ', $array);
+    }
+
+    private function getColumnName($key, $field, $settings)
+    {
+        if ( $key == 'created_at' ){
+            return _('Vytvorené dňa');
+        } else if ( $key == 'updated_at' ){
+            return _('Upravené dňa');
+        }
+
+        return @$field['name'] ?: @$settings['columns'][$key]['name'] ?: $key;
+    }
+
     public function generate()
     {
         $spreadsheet = new Spreadsheet();
@@ -53,10 +104,15 @@ class SheetDownloader
 
         $columns = array_keys($this->rows[0]);
 
+        //Allow load all options
+        $this->model->withAllOptions();
+
+        $settings = $this->model->getModelSettings();
+
         //Create header
         foreach ($columns as $i => $key) {
             $field = $this->model->getField($key);
-            $name = @$field['name'] ?: $key;
+            $name = $this->getColumnName($key, $field, $settings);
 
             $column = $this->getColumn($i);
 
@@ -67,7 +123,9 @@ class SheetDownloader
             foreach ($columns as $columnIndex => $column) {
                 $value = @$row[$column];
 
-                if ( $this->model->isFieldType($column, 'select') ){
+                if ( $this->model->hasFieldParam($column, ['belongsTo', 'belongsToMany']) ){
+                    $value = $this->getBelongsToValue($column, $value);
+                } else if ( $this->model->isFieldType($column, 'select') ){
                     $value = $this->model->getSelectOption($column, $value);
                 }
 
