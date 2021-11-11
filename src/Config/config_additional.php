2442e296410ca482cc893523578ad4f38bcd6255
Marek Gogoľ <info@marekgogol.sk>
2021-11-11 19:38:04 +0100

    iso8106 support format into request for time column

--- src/Config/config_additional.php
+++ src/Config/config_additional.php
@@ -173,7 +173,7 @@ return [
         'checkbox' => 'boolean',
         'date' => 'date_format_multiple:d.m.Y,Y-m-d,Y-m-d\TH:i:s.u\Z,Y-m-d\TH:i:sP,Y-m-d\TH:i:s.vP,Y-m-d\TH:i:s.v\Z|nullable',
         'datetime' => 'date_format_multiple:d.m.Y H:i,Y-m-d H:i,Y-m-d H:i:s,Y-m-d\TH:i:s.u\Z,Y-m-d\TH:i:sP,Y-m-d\TH:i:s.vP,Y-m-d\TH:i:s.v\Z|nullable',
-        'time' => 'date_format_multiple:H:i|nullable',
+        'time' => 'date_format_multiple:H:i,Y-m-d\TH:i:s.u\Z,Y-m-d\TH:i:sP,Y-m-d\TH:i:s.vP,Y-m-d\TH:i:s.v\Z|nullable',
         'json' => 'hidden',
     ],
 
