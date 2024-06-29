b23b6fd28e1cfca48f8b2839ef270e7288ccfca4
Marek Gogoľ <info@marekgogol.sk>
2024-06-29 20:53:06 +0200

    enum support added

--- src/Providers/FieldsServiceProvider.php
+++ src/Providers/FieldsServiceProvider.php
@@ -12,7 +12,7 @@ class FieldsServiceProvider extends ServiceProvider
 {
     protected $allFields = [
         'title', 'placeholder', 'hidden', 'orderBy', 'limit', 'multirows', 'defaultByOption', 'tooltip', 'editor_height',
-        'column_visible', 'component', 'sub_component', 'column_component', 'component_data', 'table_request_present', 'private',
+        'column_visible', 'component', 'sub_component', 'column_component', 'component_data', 'table_request_present', 'private', 'enum',
         'column_name', 'phone_link', 'ifExists', 'ifDoesntExists', 'hideOnUpdate', 'hideOnCreate', 'keepInRequest', 'encrypted',
         'inaccessible' => true, 'inaccessible_column' => true, 'invisible' => true, 'disabled' => true, 'readonly' => true,
         'removeFromForm' => true, 'hideFromForm' => true,
